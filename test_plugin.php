5cf14403906979b53e4019111a75d5035388bc22
Maarten van Gijssel <maarten@vgijssel.nl>
2014-04-07 12:13:37 +0200

    Added sample plugin placeholder

---
+++ test_plugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Test_Plugin
+ * @version 0.1
+ */
+/*
+Plugin Name: Test Plugin
+Plugin URI: http://wordpress.org/plugins/hello-dolly/
+Description: Plugin for testing the wordpress-plugin framework
+Author: Maarten van Gijssel
+Version: 0.1
+Author URI: http://localhost*/
